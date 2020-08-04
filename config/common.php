351abc59aa35e6a3d16012d208c3eab2917fdc81
denis909 <denis909@mail.ru>
2020-08-05 00:08:16 +0300

    n/a

---
+++ config/common.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'container' => [
+        'definitions' => [
+            'backend\theme\ActiveField' => 'denis909\bootstrap4\ActiveField',
+            'backend\theme\ActiveForm' => 'denis909\bootstrap4\ActiveForm',
+            'backend\theme\Breadcrumbs' => 'denis909\bootstrap4\Breadcrumbs',
+            'backend\theme\DateInput' => 'denis909\bootstrap4\DateInput',
+            'backend\theme\DateRangeInput' => 'denis909\bootstrap4\DateRangeInput',
+            'backend\theme\DateTimeInput' => 'denis909\bootstrap4\DateTimeInput',
+            'backend\theme\DetailView' => 'denis909\bootstrap4\DetailView',
+            'backend\theme\GridView' => 'denis909\bootstrap4\GridView',
+            'backend\theme\Select2' => 'denis909\bootstrap4\Select2',
+            'backend\theme\Tabs' => 'denis909\bootstrap4\Tabs',
+            'backend\theme\TimeInput' => 'denis909\bootstrap4\TimeInput'
+        ]
+    ]
+];
